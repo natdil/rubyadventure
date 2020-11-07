c71cd6f884b0e61e3bc91d80625b30d3b434de3e
Natalie <70169038+natdil@users.noreply.github.com>
2020-11-06 21:35:49 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\RubyController.cs

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -14,7 +14,7 @@ public class RubyController : MonoBehaviour
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical")
+        float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
         position.x = position.x + 0.1f * horizontal;
         transform.position = position;
