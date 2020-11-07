a73d78516b87e9157504f37983f0e615373f55d5
Natalie <70169038+natdil@users.noreply.github.com>
2020-11-07 07:20:54 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\EnemyController.cs

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float speed;
+    public float speed = 3.0f;
     public bool vertical;
-    
+    public float changeTime = 3.0f;
+
     Rigidbody2D rigidbody2D;
+    float timer;
+    int direction = 1;
     
     // Start is called before the first frame update
     void Start()
