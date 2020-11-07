0322630aa0fb67efd880b360e0bec28b036cec9d
Natalie <70169038+natdil@users.noreply.github.com>
2020-11-07 04:26:38 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\HealthCollectible.cs

--- Assets/Scripts/HealthCollectible.cs
+++ Assets/Scripts/HealthCollectible.cs
@@ -4,18 +4,17 @@ using UnityEngine;
 
 public class HealthCollectible : MonoBehaviour
 {
-void OnTriggerEnter2D(Collider2D other)
-{
-     RubyController controller = other.GetComponent<RubyController>();
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        RubyController controller = other.GetComponent<RubyController>();
 
-     if (controller != null)
-     {
-          if(controller.health < controller.maxHealth)
-          {
-	       controller.ChangeHealth(1);
-	       Destroy(gameObject);
-          }
-     }
-}
-
+        if (controller != null)
+        {
+            if(controller.health  < controller.maxHealth)
+            {
+                controller.ChangeHealth(1);
+                Destroy(gameObject);
+            }
+        }
+    }
 }
