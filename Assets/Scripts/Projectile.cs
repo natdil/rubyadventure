0069bec4139dce8d4c8ed29c2b3fc2553a3cdd25
Natalie <70169038+natdil@users.noreply.github.com>
2020-11-07 18:29:06 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\Projectile.cs

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -24,8 +24,8 @@ public class Projectile : MonoBehaviour
     {
         GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
 
-    Projectile projectile = projectileObject.GetComponent<Projectile>();
-    projectile.Launch(lookDirection, 300);
+      Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
 
     animator.SetTrigger("Launch");
 }
