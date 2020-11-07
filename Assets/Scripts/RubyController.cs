9886cd3957fcc49db6ea3c30cbdcfcb27fed6469
Natalie <70169038+natdil@users.noreply.github.com>
2020-11-06 21:36:09 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\RubyController.cs

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -18,7 +18,7 @@ public class RubyController : MonoBehaviour
 
         Vector2 position = transform.position;
         position.x = position.x + 0.1f * horizontal;
-        
+        position
         transform.position = position;
     }
 }
