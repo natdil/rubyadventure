913953fda2c762daae23ced4ecb9653ca6b98e11
Natalie <70169038+natdil@users.noreply.github.com>
2020-11-07 04:21:51 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\RubyController.cs

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -7,14 +7,14 @@ public class RubyController : MonoBehaviour
     public float speed = 3.0f;
     
     public int maxHealth = 5;
+
+    public int health { get { return currentHealth; }}
     int currentHealth;
     
     Rigidbody2D rigidbody2d;
-    float horizontal;
-    float vertical;
     
-    // Start is called before the first frame update
-      void Start()
+// Start is called before the first frame update
+    void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
