021c8a34718e9fcdc70d64364756addc1349d822
Natalie <70169038+natdil@users.noreply.github.com>
2020-11-07 04:45:52 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\DamageZone.cs

--- Assets/Scripts/DamageZone.cs
+++ Assets/Scripts/DamageZone.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class DamageZone : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
+        RubyController controller = other.GetComponent<RubyController >();
+
+        if (controller != null)
+        {
+            controller.ChangeHealth(-1);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
