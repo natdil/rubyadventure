ef420126e9647df704177c6969735cbb6478dd01
Natalie <70169038+natdil@users.noreply.github.com>
2020-11-07 04:12:08 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\RubyController.cs

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -36,7 +36,7 @@ public class RubyController : MonoBehaviour
         rigidbody2d.MovePosition(position);
     }
 
-    void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
