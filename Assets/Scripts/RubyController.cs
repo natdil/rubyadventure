ef5ec47bbb25b2c1a00ec0db0d546c8cab5c01dd
Natalie <70169038+natdil@users.noreply.github.com>
2020-11-07 02:49:37 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\RubyController.cs

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -10,7 +10,7 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
       void Start()
    { 
-       rigidbody2d
+       rigidbody2d = GetComponent
    }
    // Update is called once per frame
    void Update()
