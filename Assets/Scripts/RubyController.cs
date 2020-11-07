f1761b6192f1ef6afbe2c32954a5fee7f7f08e54
Natalie <70169038+natdil@users.noreply.github.com>
2020-11-06 21:53:23 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\RubyController.cs

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -7,7 +7,7 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        QualitySettings.vSyncCount
     }
 
     // Update is called once per frame
