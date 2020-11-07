6f0e7565fdf6492065e5946c706d0d112577844b
Natalie <70169038+natdil@users.noreply.github.com>
2020-11-07 04:07:37 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\HealthCollectible.cs

--- Assets/Scripts/HealthCollectible.cs
+++ Assets/Scripts/HealthCollectible.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class HealthCollectible : MonoBehaviour
 {
-    
-
-
-
-
-
-
-
-
-
-
+void OnTriggerEnter2D(Collider2D other)
+   {
+       Debug.Log("Object that entered the trigger : " + other);
+   }
 }
