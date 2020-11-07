d8d63107a6b054b7ae7c9348b55f80acb0c8e772
Natalie <70169038+natdil@users.noreply.github.com>
2020-11-07 07:19:08 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\EnemyController.cs

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    public float speed;
+
+    Rigidbody2D rigidbody2D;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    void FixedUpdate()
     {
+        Vector2 position = rigidbody2D.position;
+        position.x = position.x + Time.deltaTime * speed;
         
+        rigidbody2D.MovePosition(position);
     }
 }
