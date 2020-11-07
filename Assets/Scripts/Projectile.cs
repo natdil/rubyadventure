4bbfef303e4562dc5511e6e8c6cc06bebd99ca53
Natalie <70169038+natdil@users.noreply.github.com>
2020-11-07 18:24:39 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\Projectile.cs

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -7,9 +7,9 @@ public class Projectile : MonoBehaviour
    Rigidbody2D rigidbody2d;
     
     void Start()
-{
-    rigidbody2d = GetComponent<Rigidbody2D>();
-}
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
 
     // Update is called once per frame
     void Update()
