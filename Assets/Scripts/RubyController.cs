b29f9301c223f918c3d6d75f7a12130b441b7ab9
Natalie <70169038+natdil@users.noreply.github.com>
2020-11-06 21:57:12 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\RubyController.cs

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -5,21 +5,19 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 10;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        position.y = position.y + 0.1f * vertical;
-        transform.position = position;
-    }
+      void Start()
+   { 
+       //QualitySettings.vSyncCount = 0;
+       //Application.targetFrameRate = 10;
+   }
+   // Update is called once per frame
+   void Update()
+   {
+       float horizontal = Input.GetAxis("Horizontal");
+       float vertical = Input.GetAxis("Vertical");
+       Vector2 position = transform.position;
+       position.x = position.x + 0.1f * horizontal * Time.deltaTime;
+       position.y = position.y + 0.1f * vertical * Time.deltaTime;
+       transform.position = position;
+   }
 }
