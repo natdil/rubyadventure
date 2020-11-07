45b6108cee54a7e08e32f329d5113854ed92237e
Natalie <70169038+natdil@users.noreply.github.com>
2020-11-06 20:25:30 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\RubyController.cs

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ruby : MonoBehaviour
+public class RubyController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
