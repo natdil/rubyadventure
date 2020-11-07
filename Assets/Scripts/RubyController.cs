714bb94d98a7a2f7397ccd035c1441432ae15f72
Natalie <70169038+natdil@users.noreply.github.com>
2020-11-07 17:14:04 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\RubyController.cs

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -8,7 +8,7 @@ using UnityEngine;
     
     public int maxHealth = 5;
     public float timeInvincible = 2.0f;
-    
+
     public int health { get { return currentHealth; }}
     int currentHealth;
     
@@ -16,28 +16,18 @@ using UnityEngine;
     float invincibleTimer;
     
     Rigidbody2D rigidbody2d;
-    float horizontal = Input.GetAxis("Horizontal");
-    float vertical = Input.GetAxis("Vertical");
-                
-    Vector2 move = new Vector2(horizontal, vertical);
-        
-    if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
-    {
-        lookDirection.Set(move.x, move.y);
-        lookDirection.Normalize();
-    }
-        
-    animator.SetFloat("Look X", lookDirection.x);
-    animator.SetFloat("Look Y", lookDirection.y);
-    animator.SetFloat("Speed", move.magnitude);
-
+    float horizontal;
+    float vertical;
+    
     Animator animator;
     Vector2 lookDirection = new Vector2(1,0);
-
+    
+    // Start is called before the first frame update
     void Start()
     {
-    animator = GetComponent<Animator>();
         rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        
         currentHealth = maxHealth;
     }
 
@@ -46,6 +36,18 @@ using UnityEngine;
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+        
+        Vector2 move = new Vector2(horizontal, vertical);
+        
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
         
         if (isInvincible)
         {
@@ -68,6 +70,7 @@ using UnityEngine;
     {
         if (amount < 0)
         {
+            animator.SetTrigger("Hit");
             if (isInvincible)
                 return;
             
