9cb5f95fa53d38ea08ae8cf429b6e1f00d0f8090
Natalie <70169038+natdil@users.noreply.github.com>
2020-11-08 09:52:34 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\RubyController.cs

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-ppublic class RubyController : MonoBehaviour
+public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
     
