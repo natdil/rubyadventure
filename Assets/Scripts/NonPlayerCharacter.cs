87c549b25000a5d07ff65159c9006b35ba6fc776
Natalie <70169038+natdil@users.noreply.github.com>
2020-11-08 09:53:16 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\NonPlayerCharacter.cs

--- Assets/Scripts/NonPlayerCharacter.cs
+++ Assets/Scripts/NonPlayerCharacter.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 public class NonPlayerCharacter : MonoBehaviour
 {
