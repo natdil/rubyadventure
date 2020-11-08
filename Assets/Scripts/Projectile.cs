15518a8424984607cc3c2eff59cd49560224ccd5
Natalie <70169038+natdil@users.noreply.github.com>
2020-11-07 20:59:24 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\Projectile.cs

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,34 +4,34 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-   Rigidbody2D rigidbody2d;
+    Rigidbody2D rigidbody2d;
     
-    void Start()
+    void Awake()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
     }
+    
     public void Launch(Vector2 direction, float force)
     {
         rigidbody2d.AddForce(direction * force);
-    }  
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        //we also add a debug log to know what the projectile touch
-        Debug.Log("Projectile Collision with " + other.gameObject);
-        Destroy(gameObject);
     }
-    void Launch()
-    {
-        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
-
-        Projectile projectile = projectileObject.GetComponent<Projectile>();
-        projectile.Launch(lookDirection, 300);
-
-        animator.SetTrigger("Launch");
-    }
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if(transform.position.magnitude > 1000.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        EnemyController e = other.collider.GetComponent<EnemyController>();
+        if (e != null)
+        {
+            e.Fix();
+        }
+    
+        Destroy(gameObject);
     }
 }
