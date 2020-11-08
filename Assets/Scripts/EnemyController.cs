8ff80db0248440d124e9403df9531611640036fd
Natalie <70169038+natdil@users.noreply.github.com>
2020-11-07 23:17:01 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\EnemyController.cs

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
+﻿public class EnemyController : MonoBehaviour
 {
     public float speed;
     public bool vertical;
@@ -11,6 +11,7 @@ public class EnemyController : MonoBehaviour
     Rigidbody2D rigidbody2D;
     float timer;
     int direction = 1;
+    bool broken = true;
     
     Animator animator;
     
@@ -24,6 +25,12 @@ public class EnemyController : MonoBehaviour
 
     void Update()
     {
+        //remember ! inverse the test, so if broken is true !broken will be false and return won’t be executed.
+        if(!broken)
+        {
+            return;
+        }
+        
         timer -= Time.deltaTime;
 
         if (timer < 0)
@@ -35,6 +42,12 @@ public class EnemyController : MonoBehaviour
     
     void FixedUpdate()
     {
+        //remember ! inverse the test, so if broken is true !broken will be false and return won’t be executed.
+        if(!broken)
+        {
+            return;
+        }
+        
         Vector2 position = rigidbody2D.position;
         
         if (vertical)
@@ -52,12 +65,6 @@ public class EnemyController : MonoBehaviour
         
         rigidbody2D.MovePosition(position);
     }
-
-    public void Fix()
-    {
-        //broken = false;
-        rigidbody2D.simulated = false;
-    }
     
     void OnCollisionEnter2D(Collision2D other)
     {
@@ -67,5 +74,14 @@ public class EnemyController : MonoBehaviour
         {
             player.ChangeHealth(-1);
         }
+    }
+    
+    //Public because we want to call it from elsewhere like the projectile script
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2D.simulated = false;
+        //optional if you added the fixed animation
+        animator.SetTrigger("Fixed");
     }
 }
