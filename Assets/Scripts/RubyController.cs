805db9ea7a41b215898a7f14bf0e129e7c26b029
Natalie <70169038+natdil@users.noreply.github.com>
2020-11-08 09:52:27 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\RubyController.cs

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RubyController : MonoBehaviour
+ppublic class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
     
@@ -62,13 +62,18 @@ public class RubyController : MonoBehaviour
         {
             Launch();
         }
+        
         if (Input.GetKeyDown(KeyCode.X))
-{
-        RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
-        if (hit.collider != null)
         {
-        Debug.Log("Raycast has hit the object " + hit.collider.gameObject);
-             }
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if (hit.collider != null)
+            {
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                if (character != null)
+                {
+                    character.DisplayDialog();
+                }
+            }
         }
     }
     
