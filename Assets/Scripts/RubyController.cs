43af82ffe6ad0ad65f661c38879dd95810267b13
Natalie <70169038+natdil@users.noreply.github.com>
2020-11-07 23:01:37 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\RubyController.cs

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -55,6 +55,11 @@ public class RubyController : MonoBehaviour
             if (invincibleTimer < 0)
                 isInvincible = false;
         }
+
+        if(Input.GetKeyDown(KeyCode.C))
+{
+   Launch();
+}
     }
     
     void FixedUpdate()
