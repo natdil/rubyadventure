8cb904ac1a7b6355d0d3d0504d8d23fc6795d14c
Natalie <70169038+natdil@users.noreply.github.com>
2020-11-07 21:34:31 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\HealthCollectible.cs

--- Assets/Scripts/HealthCollectible.cs
+++ Assets/Scripts/HealthCollectible.cs
@@ -6,6 +6,7 @@ public class HealthCollectible : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("I see you");
         RubyController controller = other.GetComponent<RubyController>();
 
         if (controller != null)
