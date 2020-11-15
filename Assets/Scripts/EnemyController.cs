8550ee5d6551d79efde669fde978a3f59c6ff0da
Natalie <70169038+natdil@users.noreply.github.com>
2020-11-15 18:04:35 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\EnemyController.cs

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -89,6 +89,6 @@ public class EnemyController : MonoBehaviour
         animator.SetTrigger("Fixed");
         
         smokeEffect.Stop();
-        Destroy.smokeEffect(smokeEffect, true);
+        Destroy.smokeEffect(smokeEffect);
     }
 }
