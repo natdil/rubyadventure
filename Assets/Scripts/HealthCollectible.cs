a544e2458da099de1aef4a21165328f5fff2bdbd
Natalie <70169038+natdil@users.noreply.github.com>
2020-11-15 18:14:54 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\HealthCollectible.cs

--- Assets/Scripts/HealthCollectible.cs
+++ Assets/Scripts/HealthCollectible.cs
@@ -15,7 +15,7 @@ public class HealthCollectible : MonoBehaviour
             if (controller.health < controller.maxHealth)
             {
                 controller.ChangeHealth(1);
-                 Instantiate()
+                Instantiate()
                 Destroy(gameObject);
                
                 controller.PlaySound(collectedClip);
