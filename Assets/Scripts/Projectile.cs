3d0d2ddb0a8a931ea2a493a55324dda627f31906
Natalie <70169038+natdil@users.noreply.github.com>
2020-12-03 18:41:40 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\Projectile.cs

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -42,7 +42,7 @@ public class Projectile : MonoBehaviour
 
             
         Destroy(gameObject);
-        fixedCount ++;
+        fixedCount += 1;
         FixedText.text = "Fixed Robots: " ;
     }
 }
