dcf8d78362ea552b6d331b6cff79400d3aa556b4
Natalie <70169038+natdil@users.noreply.github.com>
2020-12-03 18:01:42 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\FixedRobotText.cs

--- Assets/Scripts/FixedRobotText.cs
+++ Assets/Scripts/FixedRobotText.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class FixedRobotText : MonoBehaviour
 {
-    public Text FixedRText;
+    public Text FixedRobotText;
     // Start is called before the first frame update
     void Start()
     {
