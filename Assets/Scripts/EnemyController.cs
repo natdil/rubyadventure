01696aa2fdbfdf23c78209f8524499b3090ebd78
Natalie <70169038+natdil@users.noreply.github.com>
2020-12-03 18:56:29 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\EnemyController.cs

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -19,6 +19,8 @@ public class EnemyController : MonoBehaviour
     bool broken = true;
     
     Animator animator;
+
+    private RubyController rubyController;
     
     // Start is called before the first frame update
     void Start()
