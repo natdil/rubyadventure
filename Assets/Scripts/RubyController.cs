753dc25fd2abb39a3f21a5ca55991ed62db21b44
Natalie <70169038+natdil@users.noreply.github.com>
2020-12-03 16:45:00 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\RubyController.cs

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -110,7 +110,7 @@ public class RubyController : MonoBehaviour
             invincibleTimer = timeInvincible;
             
             PlaySound(hitSound);
-            damageEffect.startColor()
+            damageEffect.Start
         }
         
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
