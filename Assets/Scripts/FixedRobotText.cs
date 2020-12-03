eac73f2a4736b527d308884e47efe3c21c4c3410
Natalie <70169038+natdil@users.noreply.github.com>
2020-12-03 18:34:58 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\FixedRobotText.cs

--- Assets/Scripts/FixedRobotText.cs
+++ Assets/Scripts/FixedRobotText.cs
@@ -10,7 +10,7 @@ public class FixedRobotText : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        fixed
     }
 
     // Update is called once per frame
