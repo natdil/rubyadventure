2d7139f50559bf7706d4278fe0d17fb34f5efee0
Natalie <70169038+natdil@users.noreply.github.com>
2020-12-03 18:01:14 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\EnemyController.cs

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -11,7 +11,7 @@ public class EnemyController : MonoBehaviour
     public float changeTime = 3.0f;
 
     public ParticleSystem smokeEffect;
-    public Text Fixed
+    public Text FixedRobotText;
     
     Rigidbody2D rigidbody2D;
     float timer;
