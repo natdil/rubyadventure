f688fd89e900a0b49de603a1d02356637b0732c4
Natalie <70169038+natdil@users.noreply.github.com>
2020-12-04 00:48:11 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\RubyController.cs

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -166,7 +166,7 @@ public class RubyController : MonoBehaviour
        
         currentScore = currentScore + 1;
         fixedRobotsText.text = "Score: " + currentScore;
-        if (currentScore >= 5)
+        if (currentScore >= 8)
         {
             winText.text = "You Did It! Game built by Natalie Dilbeck";
            
