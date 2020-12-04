b2638b6614813521d18e378dde4ded991d227be7
Natalie <70169038+natdil@users.noreply.github.com>
2020-12-03 20:59:58 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\RubyController.cs

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -126,6 +126,7 @@ public class RubyController : MonoBehaviour
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         
         UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
+        if
     }
 
     public void ChangeScore(int amount)
