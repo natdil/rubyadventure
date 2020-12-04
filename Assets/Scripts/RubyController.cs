b45068dd4683554eaaf5d848f2101dbcc87c73d6
Natalie <70169038+natdil@users.noreply.github.com>
2020-12-03 20:49:59 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\RubyController.cs

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -48,7 +48,7 @@ public class RubyController : MonoBehaviour
         currentHealth = maxHealth;
 
         audioSource = GetComponent<AudioSource>();
-        winText.Text
+        winText.Text = " "
     }
 
     // Update is called once per frame
