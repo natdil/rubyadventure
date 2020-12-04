b5e8829ac184c56a80068753a396aca995956999
Natalie <70169038+natdil@users.noreply.github.com>
2020-12-04 00:11:11 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\PyaraScript.cs

--- Assets/Scripts/PyaraScript.cs
+++ Assets/Scripts/PyaraScript.cs
@@ -14,7 +14,7 @@ public class PyaraScript : MonoBehaviour
 
         if (controller != null)
         {
-            if (On)
+            if (OnTriggerEnter2D)
             {
                 Destroy(gameObject);
             
