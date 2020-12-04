3ff1d331ffcd93a69612a57f379fa583ebbb4a26
Natalie <70169038+natdil@users.noreply.github.com>
2020-12-04 00:26:41 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\PyaraScript.cs

--- Assets/Scripts/PyaraScript.cs
+++ Assets/Scripts/PyaraScript.cs
@@ -31,6 +31,7 @@ public class PyaraScript : MonoBehaviour
 
      public void ChangeScore(int amount)
     {
+        fixedRobotsText.text = "Score: " + currentScore;
         currentScore = currentScore + 1;
     }
 }
