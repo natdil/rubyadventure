7cfc5926ed7ab5abaebbf3ef7771481f9dc39533
Natalie <70169038+natdil@users.noreply.github.com>
2020-12-03 22:08:44 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\RubyController.cs

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -13,7 +13,7 @@ public class RubyController : MonoBehaviour
     public Text winText;
     public Text loseText;
     bool gameOver;
-    public Scene
+    public SceneManager;
 
     public int currentScore;
     
