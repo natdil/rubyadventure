c68280ac6e32791797d739c94688f9b33725ab27
Natalie <70169038+natdil@users.noreply.github.com>
2020-12-04 00:42:21 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\RubyController.cs

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -155,10 +155,7 @@ public class RubyController : MonoBehaviour
 
     public void ChangeScore(int amount)
     {
-        
-
- 
-}
+       
         currentScore = currentScore + 1;
         fixedRobotsText.text = "Score: " + currentScore;
         if (currentScore >= 5)
