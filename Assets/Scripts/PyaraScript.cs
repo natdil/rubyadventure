854737adecaa49dc32f59e80635fc912d97c138f
Natalie <70169038+natdil@users.noreply.github.com>
2020-12-04 00:00:22 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\PyaraScript.cs

--- Assets/Scripts/PyaraScript.cs
+++ Assets/Scripts/PyaraScript.cs
@@ -6,7 +6,7 @@ public class PyaraScript : MonoBehaviour
 {
     public AudioClip missionComplete;
     public ParticleSystem love;
-    public 
+    public GameObject
     
     void OnTriggerEnter2D(Collider2D other)
     {
