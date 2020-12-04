dbbd771d76bfe6c02e50b36e842d42a3c852045d
Natalie <70169038+natdil@users.noreply.github.com>
2020-12-04 01:58:18 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\BigHealthScript.cs

--- Assets/Scripts/BigHealthScript.cs
+++ Assets/Scripts/BigHealthScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthCollectible : MonoBehaviour
+public class BigHealth : MonoBehaviour
 {
     public AudioClip collectedClip;
     public ParticleSystem healthEffect;
