eef341822d29c1020912ed561f131d38503a1a94
Natalie <70169038+natdil@users.noreply.github.com>
2020-12-04 00:27:11 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\PyaraScript.cs

--- Assets/Scripts/PyaraScript.cs
+++ Assets/Scripts/PyaraScript.cs
@@ -8,7 +8,7 @@ public class PyaraScript : MonoBehaviour
     public ParticleSystem love;
     public GameObject kissingBooth;
     public int currentScore;
-    public text 
+    public text fixedRob
     
     void OnTriggerEnter2D(Collider2D kissingBooth)
     {
