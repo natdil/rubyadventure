5ccfdde2e0498207a5da2851a2f9725eb177e028
Natalie <70169038+natdil@users.noreply.github.com>
2020-12-04 00:37:52 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\PyaraScript.cs

--- Assets/Scripts/PyaraScript.cs
+++ Assets/Scripts/PyaraScript.cs
@@ -25,7 +25,7 @@ public class PyaraScript : MonoBehaviour
                 controller.PlaySound(missionComplete);
                 
                 love.Stop();
-                if (controller.PlaySound)
+                if (controller.PlaySound(missionComplete))
                 pyaraKissed = true;
         
         }
