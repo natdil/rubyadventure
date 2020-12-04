9d93255e2a638740d519c028f8d988561e3a4552
Natalie <70169038+natdil@users.noreply.github.com>
2020-12-04 01:29:02 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\RubyController.cs

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -148,7 +148,7 @@ public class RubyController : MonoBehaviour
         UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
         if (currentHealth <= 0)
         {
-            loseText.text = "Whoops! She is dead :/";
+            loseText.text = "Whoops! She is dead :/ Try again Later";
             speed = 0;
             PlaySound(deathSound);
         }
