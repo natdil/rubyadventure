d015ad8729e3bfde58da31682c73f08a0efa7a91
Natalie <70169038+natdil@users.noreply.github.com>
2020-12-04 01:58:04 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\BigHealthScript.cs

--- Assets/Scripts/BigHealthScript.cs
+++ Assets/Scripts/BigHealthScript.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BigHealthScript : MonoBehaviour
+public class HealthCollectible : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioClip collectedClip;
+    public ParticleSystem healthEffect;
+    
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
+        RubyController controller = other.GetComponent<RubyController>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (controller != null)
+        {
+            if (controller.health < controller.maxHealth)
+            {
+                controller.ChangeHealth(1);
+                Destroy(gameObject);
+            
+                controller.PlaySound(collectedClip);
+                
+                healthEffect.Stop();
+            }
+        }
+
     }
 }
