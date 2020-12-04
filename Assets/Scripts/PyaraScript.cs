b41894c05b59f2f6699a3d06e5ac769ce50511e2
Natalie <70169038+natdil@users.noreply.github.com>
2020-12-03 23:57:52 -0500

    Modified c:\Users\ndilb\Desktop\unity\ruby\tutorial-3-rubys-adventure-natdil\Assets\Scripts\PyaraScript.cs

--- Assets/Scripts/PyaraScript.cs
+++ Assets/Scripts/PyaraScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pyara : MonoBehaviour
+public class PyaraScript : MonoBehaviour
 {
     public AudioClip collectedClip;
     public ParticleSystem healthEffect;
